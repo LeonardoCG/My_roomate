6290575d56318e42b4615e6ae8bcbdc421b0b17b
cchab <omar.chab@embsoft.com.mx>
2020-03-26 23:50:18 -0600

    Corrección de action bar

--- app/src/main/java/com/example/my_roomate/menu_nav.java
+++ app/src/main/java/com/example/my_roomate/menu_nav.java
@@ -1,10 +1,14 @@
 package com.example.my_roomate;
 
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -16,9 +20,11 @@ public class menu_nav extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setTheme(R.style.AppTheme);//Ajustamos para que este sea el tema a cargar
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_menu_nav);
+        setTheme(R.style.AppTheme);//Ajustamos para que este sea el tema a cargar
+
+        hideActionBar(true);
+
         BottomNavigationView navView = findViewById(R.id.nav_view);
         //getSupportActionBar().hide();
         // Passing each menu ID as a set of Ids because each
@@ -27,8 +33,23 @@ public class menu_nav extends AppCompatActivity {
                 R.id.navigation_myrooms, R.id.navigation_buzon, R.id.navigation_explorar, R.id.navigation_perfil, R.id.navigation_guardados)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        //NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+    }
+
+    public void hideActionBar(boolean hide) {
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            if (hide) {
+                actionBar.hide();
+            } else {
+                actionBar.show();
+            }
+        }
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
+
+
     }
 
 }
