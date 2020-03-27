a4754e3926832c3c4fc5afc8c5f409afbb58a6b9
Jayro_Salazar <megajjks@gmail.com>
2020-03-27 00:36:12 -0600

    Update add Perfil -Se creo la vista y recursos de Perfil

--- app/src/main/java/com/example/my_roomate/ui/perfil/PerfilFragment.java
+++ app/src/main/java/com/example/my_roomate/ui/perfil/PerfilFragment.java
@@ -4,32 +4,42 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.my_roomate.R;
 import com.example.my_roomate.ui.perfil.PerfilViewModel;
+import com.example.my_roomate.ui.propuesta.PropuestaAdd;
 
 public class PerfilFragment extends Fragment {
     private PerfilViewModel perfilViewModel;
+    private ImageButton btn_setting;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         perfilViewModel =
                 ViewModelProviders.of(this).get(PerfilViewModel.class);
         View root = inflater.inflate(R.layout.fragment_perfil, container, false);
-        final TextView textView = root.findViewById(R.id.text_perfil);
-        perfilViewModel.getText().observe(this, new Observer<String>() {
+
+        //conectamos xml con class
+        btn_setting = root.findViewById(R.id.settings_global);
+
+        //Escuchadores
+        btn_setting.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "Cooming soon", Toast.LENGTH_LONG).show();
             }
         });
+
         return root;
     }
 }
